cebf9fa7289f0ecf8436d883e650af997c913c98
Bernhard Vallant <bernhard@vallant.org>
2016-02-21 19:20:56 +0100

    project permission view check

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url, include
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+
+from . import views
 
 
 project_patterns = [
-    url(r'^$', TemplateView.as_view(template_name='accounts/project_list.html'),
+    url(r'^$', login_required(TemplateView.as_view(template_name='accounts/project_list.html')),
         name='project-list'),
+    url(r'^(?P<project_id>[A-Za-z0-9]+)/$', views.ProjectDetail.as_view(),
+        name='project-detail')
 ]
 
 
